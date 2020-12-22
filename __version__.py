ca40bab01fe2cfbab86ea973bb5890cff11585da
iromli <isman.firmansyah@gmail.com>
2020-12-23 01:55:23 +0700

    feat(release): bump version to 4.2.2_02

--- __version__.py
+++ __version__.py
@@ -3,5 +3,5 @@
  https://www.apache.org/licenses/LICENSE-2.0
  This is only used by Github actions: release.yaml workflow.
 """
-__version__ = "4.2.2_01"
-__previous_version__ = "4.2.1_02"
+__version__ = "4.2.2_02"
+__previous_version__ = "4.2.2_01"
