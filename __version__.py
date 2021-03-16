1e1708a95cc09401a0746fdede13a3bfacf1cdf3
iromli <isman.firmansyah@gmail.com>
2021-03-16 23:51:22 +0700

    build(release): bump version to 4.2.3_04

--- __version__.py
+++ __version__.py
@@ -3,5 +3,5 @@
  https://www.apache.org/licenses/LICENSE-2.0
  This is only used by Github actions: release.yaml workflow.
 """
-__version__ = "4.2.3_03"
-__previous_version__ = "4.2.3_02"
+__version__ = "4.2.3_04"
+__previous_version__ = "4.2.3_03"
